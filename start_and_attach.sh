3608ad9806fc9a9bad9f97c85b39622451fbf737
Paolo Viotti <paolo.viotti@lip6.fr>
2018-02-19 17:23:21 +0100

    tail -F

--- start_and_attach.sh
+++ start_and_attach.sh
@@ -4,7 +4,7 @@ NPROC=$(nproc)
 
 if [ "$NPROC" -ge 2 ]; then
   trap "echo 'Shutting down' && /opt/antidote/bin/env stop" TERM
-  /opt/antidote/bin/env start && sleep 10 && tail -f /opt/antidote/log/console.log & wait ${!}
+  /opt/antidote/bin/env start && sleep 10 && tail -F /opt/antidote/log/console.log & wait ${!}
 else
   echo "You need at least 2 cpu cores in your Docker (virtual) machine to run this image!"
   echo "You have ${NPROC} processors"
